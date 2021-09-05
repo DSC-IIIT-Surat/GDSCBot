a437c24be106c1bf2f7db87947a81d27bcdfa62f
AvanishCodes <avanishcodes@gmail.com>
2021-09-05 16:10:52 +0530

    Library level refactoring of the existing code

--- about/__init__.py
+++ about/__init__.py
@@ -8,7 +8,12 @@ load_dotenv()
 PROFILE_PIC = getenv('PROFILE_PIC')
 
 # Function to return an about embed
-def get_about():
+
+
+def get_about() -> discord.Embed:
+    '''
+    Returns an about embed
+    '''
     randomIndex = randint(
         0, len(colors) - 1)    # get a random color
     about_msg = discord.Embed(title="This bot is for managing the Discord server of Google Developer Students Club, Indian Institute of Information technoology, Surat.",
@@ -18,7 +23,7 @@ def get_about():
     about_msg.set_thumbnail(
         url=PROFILE_PIC)
     about_msg.add_field(
-        name='Contribute', value='[To add your thoughts/code, or to make bugfixes, visit GitHub](https://twitter.com/dsc_iiitsurat)')
+        name='Contribute', value='[To add your thoughts/code, or to make bugfixes, visit GitHub](https://github.com/DSC-IIIT-Surat/GDSCBot/issues)')
     about_msg.set_footer(
         text='Collaborate and Contribute', icon_url=PROFILE_PIC)
     return about_msg
--- jokes/__init__.py
+++ jokes/__init__.py
@@ -1,3 +1,4 @@
+import discord
 import requests
 from random import randint
 import json
@@ -8,9 +9,14 @@ from colors import colors
 # Function to get a random joke
 
 
-def get_joke(jokeType='general', number=1):
+def get_joke(jokeType='general', number=1) -> discord.Embed:
     '''
         Return a joke Embed to the calling variable
+        Parameters:
+            jokeType: Type of joke to fetch
+            number: Number of jokes to fetch
+        Returns:
+            A joke Embed
     '''
     base_url = "https://official-joke-api.appspot.com"
     response = None
@@ -26,8 +32,6 @@ def get_joke(jokeType='general', number=1):
         else:
             response = requests.get(f"{base_url}/jokes/programming/random")
     jokes = json.loads(response.text)
-    for item in jokes:
-        print(item)
     quote = ""
     for joke in jokes:
         new_quote = joke['setup'] + " \n> " + joke['punchline'] + "\n"
@@ -42,13 +46,9 @@ def get_joke(jokeType='general', number=1):
     joke_embed.set_thumbnail(
         url='https://pbs.twimg.com/profile_images/1304114355517173769/F1e86tGu_400x400.jpg')
     joke_embed.add_field(
-        name='Contribute', value='[To add your thoughts/code, or to make bugfixes, visit GitHub](https://twitter.com/dsc_iiitsurat)')
+        name='Contribute', value='[To add your thoughts/code, or to make bugfixes, visit GitHub](https://github.com/DSC-IIIT-Surat/GDSCBot/issues)')
     joke_embed.set_footer(
         text='Collaborate and Contribute', icon_url='https://pbs.twimg.com/profile_images/1304114355517173769/F1e86tGu_400x400.jpg')
     print(quote)
     # await message.reply(embed=about_msg)
     return joke_embed
-
-
-if __name__ == "__main__":
-    print(get_joke())
